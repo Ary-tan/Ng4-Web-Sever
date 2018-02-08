e8f32c6bc209d7f1706ec92a0f0bbbcb2767df70
Art-tan <1121857865@qq.com>
2018-02-08 23:07:28 +0800

    配置路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,43 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router'; // 引入路由
 import {HttpClientModule} from '@angular/common/http';
 import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {BannerComponent} from './components/banner/banner.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {ServerComponent} from './components/server/server.component';
 
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: BannerComponent,
+    },
+    {
+        path: 'server',
+        component: ServerComponent
+    }
+];
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
+        FooterComponent,
         BannerComponent,
-        FooterComponent
+        ServerComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        RouterModule.forRoot(
+            appRoutes,
+            {enableTracing: true}
+        ),
         NgZorroAntdModule.forRoot()
     ],
     bootstrap: [AppComponent]
